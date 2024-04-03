03d0aeb4d024f22acbb5b40c0f41c89bb8e5e73f
bebemugisha <mugishab2003@gmail.com>
2024-04-03 05:54:25 -0700

    update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://doctorappointment-zo3m.onrender.com/api/v1'
+export const BASE_URL = 'https://doctor-appointment-project.onrender.com/api/v1'
 export const token = localStorage.getItem('token');
--- frontend/src/hooks/useFetchData.jsx
+++ frontend/src/hooks/useFetchData.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { token } from '../config';
+import { useEffect, useState } from 'react'
+import { token } from '../config'
 
 const useFetchData = (url) => {
   const [data, setData] = useState([])
@@ -7,16 +7,15 @@ const useFetchData = (url) => {
   const [error, setError] = useState(null)
 
   useEffect(() => {
-
     const fetchData = async () => {
       setLoading(true)
 
       try {
         const res = await fetch(url, {
           headers: { Authorization: `Bearer ${token}` },
-        });
+        })
 
-        const result = await res.json();
+        const result = await res.json()
 
         if (!res.ok) {
           throw new Error(result.message + '🤢')
